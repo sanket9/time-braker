77ed42a956d22fabd007f39c2c433343c037311f
Sanket Mukherjee <sanket@eccentricengine.co>
2020-05-06 01:23:00 +0530

    codebase commit ⏲⏲

---
+++ main.js
@@ -0,0 +1,106 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, Tray } = require("electron");
+const path = require("path");
+const nativeImage = require("electron").nativeImage;
+
+let appIcon = null;
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    transparent: true,
+    frame: false,
+    center: true,
+    show: false,
+    alwaysOnTop: true,
+    fullscreenable: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // mainWindow.maximize();
+  mainWindow.loadFile(path.join(__dirname, "src/index.html"));
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools({ mode: "detach" });
+
+  mainWindow.on("hide", () => {
+    console.log("Executed..");
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+// app.whenReady().then(createWindow);
+app.whenReady().then(crateSettingWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin" || process.platform !== "win32") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+app.on("ready", () => {
+  var iconPath = path.join(__dirname, "/images/tray-icon.png");
+  let trayIcon = nativeImage.createFromPath(iconPath);
+  appIcon = new Tray(trayIcon);
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Settings",
+      click: () => {
+        crateSettingWindow();
+      },
+    },
+    {
+      label: "Quit",
+      click: () => {
+        console.log("Quit Clicked");
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+  appIcon.setToolTip("Time Braker");
+  appIcon.setContextMenu(contextMenu);
+});
+
+/* Create  Settings Window*/
+function crateSettingWindow() {
+  const settingsWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true,
+    center: true,
+    show: true,
+    autoHideMenuBar: true,
+    fullscreenable: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  settingsWindow.loadFile(path.join(__dirname, "src/settings.html"));
+  // Open the DevTools.
+  settingsWindow.webContents.openDevTools({ mode: "detach" });
+
+  settingsWindow.on("closed", () => {
+    console.log("Settings Colosed..");
+  });
+}
---
+++ src/renderer.js
@@ -0,0 +1,173 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+const remote = require("electron").remote;
+const $ = require("jquery");
+const moment = require("moment");
+const path = require("path");
+const nativeImage = require("electron").nativeImage;
+
+let progressBarWidth = 0;
+let snoozeStartTime = null;
+let snoozeFor;
+let isTimerPopupOn = false;
+const alreadyTimeShownFor = {
+  eye: false,
+  water: false,
+  stretch: false,
+};
+const elem = document.getElementById("timeBar");
+let courentTime = null;
+const cardText = document.querySelector(".card-text");
+const img = document.querySelector("img");
+const headingText = document.querySelector(".text-dark");
+const btn1 = document.getElementById("snooze-1-btn");
+const btn2 = document.getElementById("snooze-2-btn");
+
+let activeTypeBrake = "";
+const textObj = {
+  eye: "Take Eye Brake.",
+  water: "Water Brake.",
+  stretch: "Quick Stretch.",
+};
+var timeCounter = {
+  backGroundTimer: null,
+  modalTimeInterval: null,
+  snoozeTimeInterval: null,
+};
+
+$("#close-btn").on("click", (e) => {
+  remote.getCurrentWindow().hide();
+  elem.style.width = 0 + "%";
+  clearInterval(timeCounter.modalTimeInterval);
+  if (activeTypeBrake == "stretch") {
+    courentTime = moment().format();
+    allwaysBackProcess();
+  }
+});
+
+btn1.addEventListener("click", (e) => {
+  e.preventDefault();
+  remote.getCurrentWindow().hide();
+  snoozeStartTime = moment();
+  elem.style.width = 0 + "%";
+  snoozeFor = 1;
+  clearInterval(timeCounter.modalTimeInterval);
+  clearInterval(timeCounter.backGroundTimer);
+  checkSnoozeTimeComplete();
+});
+btn2.addEventListener("click", (e) => {
+  e.preventDefault();
+  remote.getCurrentWindow().hide();
+  snoozeStartTime = moment();
+  elem.style.width = 0 + "%";
+  snoozeFor = 2;
+  clearInterval(timeCounter.modalTimeInterval);
+  clearInterval(timeCounter.backGroundTimer);
+  checkSnoozeTimeComplete();
+});
+
+async function initApp() {
+  courentTime = moment().format();
+  localStorage.setItem("appOntime", courentTime);
+  allwaysBackProcess();
+}
+
+function allwaysBackProcess() {
+  // elem.style.width = 0 + "%";
+  timeCounter.backGroundTimer = setInterval(() => {
+    let end = moment();
+    let duration = moment.duration(end.diff(courentTime));
+    console.log("time deff", duration.asMinutes().toFixed(2) / 20);
+    let timeDeff = (duration.asMinutes().toFixed(2) / 20).toFixed(1);
+    if (timeDeff == 1.0) {
+      if (!alreadyTimeShownFor.eye) {
+        activeTypeBrake = "eye";
+        remote.getCurrentWindow().show();
+        showinterValWithTime(10);
+        alreadyTimeShownFor.eye = true;
+      }
+      // clearInterval(backGroundTimer);
+    } else if (timeDeff == 3.0) {
+      if (!alreadyTimeShownFor.water) {
+        activeTypeBrake = "water";
+        remote.getCurrentWindow().show();
+        showinterValWithTime(60);
+        alreadyTimeShownFor.water = true;
+      }
+      // clearInterval(backGroundTimer);
+    } else if (timeDeff == 6.0) {
+      if (!alreadyTimeShownFor.stretch) {
+        remote.getCurrentWindow().show();
+        activeTypeBrake = "stretch";
+        showinterValWithTime(120);
+        clearInterval(timeCounter.backGroundTimer);
+        alreadyTimeShownFor.stretch = true;
+      }
+    }
+  }, 60000);
+}
+
+function showinterValWithTime(time) {
+  let timeInterval = 100 / Number(time);
+  let remainTime = 0;
+  if (activeTypeBrake == "stretch") {
+    let iconPath = path.join(__dirname, "/images/stretch.gif");
+    const image = nativeImage.createFromPath(iconPath);
+
+    img.src = iconPath;
+  } else if (activeTypeBrake == "eye") {
+    let iconPath = path.join(__dirname, "/images/tenor.gif");
+    const image = nativeImage.createFromPath(iconPath);
+    img.src = iconPath;
+  } else {
+    let iconPath = path.join(__dirname, "/images/drink-water.jpg");
+    const image = nativeImage.createFromPath(iconPath);
+    img.src = iconPath;
+  }
+  timeCounter.modalTimeInterval = setInterval(function () {
+    headingText.innerText = textObj[activeTypeBrake];
+    cardText.innerText = `Time Left: ${Number(time) - remainTime}Sec`;
+    if (progressBarWidth >= 101) {
+      clearInterval(timeCounter.modalTimeInterval);
+      isTimerPopupOn = false;
+      progressBarWidth = 0;
+      elem.style.width = progressBarWidth + "%";
+      remote.getCurrentWindow().hide();
+      if (activeTypeBrake == "stretch") {
+        console.log("Time reset..");
+        alreadyTimeShownFor.water = false;
+        alreadyTimeShownFor.eye = false;
+        alreadyTimeShownFor.stretch = false;
+        courentTime = moment().format();
+        allwaysBackProcess();
+      }
+    } else {
+      progressBarWidth = progressBarWidth + timeInterval;
+      // console.log(progressBarWidth);
+
+      elem.style.width = progressBarWidth + "%";
+      remainTime++;
+    }
+  }, 1000);
+}
+
+function checkSnoozeTimeComplete() {
+  timeCounter.snoozeTimeInterval = setTimeout(() => {
+    let end = moment();
+    let duration = moment.duration(end.diff(courentTime));
+    console.log("snooze....", duration.asHours());
+    console.log("snooze....for", snoozeFor);
+
+    if (duration.asHours() == snoozeFor) {
+      console.log("snooze....for casncel", duration.asHours());
+      clearInterval(timeCounter.snoozeTimeInterval);
+      initApp();
+    }
+  }, 60000);
+}
+
+initApp();
